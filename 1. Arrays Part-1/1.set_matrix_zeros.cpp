3cb7a701ce674522366fb58b70b574ceb71d1d46
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-07-02 19:32:28 +0530

    Striver

--- 1. Arrays Part-1/1.set_matrix_zeros.cpp
+++ 1. Arrays Part-1/1.set_matrix_zeros.cpp
@@ -3,55 +3,34 @@
 
 using namespace std;
 
-void setMatrixZeros(vector<vector<int> > &matrix) {
-    int m = matrix.size();
-    int n = matrix[0].size();
-    bool row = false, col = false;
-    for (int i = 0; i < m; i++) {
-        if (matrix[i][0] == 0) {
-            col = true;
-            break;
-        }
-    }
-    for (int j = 0; j < n; j++) {
-        if (matrix[0][j] == 0) {
-            row = true;
-            break;
-        }
-    }
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < n; j++)
-            if (matrix[i][j] == 0) {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
+class solution{
+    public:
+    
+    void setMatrixZeros(vector<vector<int>> &matrix){
+        int col0 = 1;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for (int i=0; i<rows; i++){
+            if (matrix[i][0]==0) col0=0;
+            for (int j=1; j<cols; j++){
+                if (matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
             }
-    }
-    for (int i = 1; i < m; i++) {
-        if (matrix[i][0] == 0)
-            for (int j = 1; j < n; j++)
-                matrix[i][j] = 0;
-    }
-    for (int j = 1; j < n; j++) {
-        if (matrix[0][j] == 0)
-            for (int i = 1; i < m; i++)
-                matrix[i][j] = 0;
-    }
-    if (row)
-        for (int j = 0; j < n; j++)
-            matrix[0][j] = 0;
-    if (col)
-        for (int i = 0; i < m; i++)
-            matrix[i][0] = 0;
-}
+        }
 
-int main(){
-    vector<vector<int> > matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-    setMatrixZeros(matrix);
-    for (auto row : matrix) {
-        for (auto col : row)
-            cout << col << " ";
-        cout << endl;
+        for (int i=rows-1; i>=0; i--){
+            for (int j=cols-1; j>=1; j--){
+                if (matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if (col0==0) matrix[i][0]=0;
+        }
     }
-    return 0;
-}
+};
 
+// Time Complexity: O(number of rows * number of columns), single pass
+// Space Complexity: O(1), no extra space used.
