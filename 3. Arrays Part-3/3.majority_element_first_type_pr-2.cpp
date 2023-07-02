5d129935c9cb8434cb12a1e51b0798941e686466
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-07-02 19:04:18 +0530

    Striver

--- 3. Arrays Part-3/3.majority_element_first_type_pr-2.cpp
+++ 3. Arrays Part-3/3.majority_element_first_type_pr-2.cpp
@@ -6,3 +6,23 @@
 
 using namespace std;
 
+class solution {
+    public:
+
+    int majorityElement(vector<int> &nums){
+        map<int, int> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]++];
+        }
+
+        for (auto it: mpp){
+            if (it.second> nums.size()/2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+
+// Time Complexity: O(nlogn), due to the use of map. Time complexity of map is O(logn) for insertion and search.
+// Extra Space used for map for n elements. Space Complexity: O(n).
