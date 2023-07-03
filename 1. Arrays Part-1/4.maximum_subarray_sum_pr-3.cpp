01dc76dfbdccfd811f8920f8b6a0d1e74d15f553
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-07-03 21:05:49 +0530

    Kadane's Algorithm

---
+++ 1. Arrays Part-1/4.maximum_subarray_sum_pr-3.cpp
@@ -0,0 +1,23 @@
+// Kadane's Algorithm
+
+#include <iostream>
+
+using namespace std;
+
+class solution{
+    public:
+
+    int maxSubnumsaySum(vector<int> &nums){
+
+        int n = nums.size();
+        int max_so_far = nums[0];
+        int curr_max = nums[0];
+
+        for(int i=1; i<n; i++){
+            curr_max = max(nums[i], curr_max+nums[i]);
+            max_so_far = max(max_so_far, curr_max);
+        }
+        return max_so_far;
+    }
+
+};
