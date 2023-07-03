df048a4ccfea2f44c746ef81f3383fd6f80ba09f
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-07-03 23:58:49 +0530

    Pascals Triangle Brute Force

---
+++ 1. Arrays Part-1/2.pascals_triangle.cpp
@@ -0,0 +1,27 @@
+// Pascals Triangle Brute Force
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class solution{
+    public:
+
+    vector<vector<int>> pascalsTriangle(int n){
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++){
+            vector<int> row;
+            for(int j=0; j<=i; j++){
+                if(j==0 || j==i){
+                    row.push_back(1);
+                }
+                else{
+                    row.push_back(ans[i-1][j-1] + ans[i-1][j]);
+                }
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
