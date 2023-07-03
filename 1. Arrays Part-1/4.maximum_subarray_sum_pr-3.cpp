32fa7064c7fca6b264d682afe9bf4281b626ceee
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-07-03 22:37:27 +0530

    ..

--- 1. Arrays Part-1/4.maximum_subarray_sum_pr-3.cpp
+++ 1. Arrays Part-1/4.maximum_subarray_sum_pr-3.cpp
@@ -9,9 +9,15 @@ class solution{
 
     int maxSubnumsaySum(vector<int> &nums){
 
-        int n = nums.size();
-        int max_so_far = nums[0];
-        int curr_max = nums[0];
+        // int n = nums.size();
+        // int max_so_far = nums[0];
+        // int curr_max = nums[0];
+
+        // for(int i=1; i<n; i++){
+        //     curr_max = max(nums[i], curr_max+nums[i]);
+        //     max_so_far = max(max_so_far, curr_max);
+        // }
+        // return max_so_far;
 
         // process-2
         int n = nums.size();
